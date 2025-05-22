8bed6906f96b6f2a8f9ff1fc5b5c2e8649f0c60e
daril.acuna <daril.acuna@estudiantes.utec.edu.uy>
2025-05-22 17:52:54 -0300

    Creacion de clase usuario

--- CasoEstudioUTEC/src/Usuario.java
+++ CasoEstudioUTEC/src/Usuario.java
@@ -1,2 +1,43 @@
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    // Constructor
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Usuario: " + nombre + " " + apellido + ", Email: " + email;
+    }
 }
